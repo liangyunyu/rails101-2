fa6377451905c25cd9c9a8f40fb470a0a517a25a
lyy <liangyunyu@gmail.com>
2017-03-14 09:51:31 +0800

    edit bootstrap flash function

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,7 @@
 class WelcomeController < ApplicationController
+  def index
+    flash[:notice] = "notice:早安！快起床！"
+    flash[:alert] = "alert:晚安！该睡了！"
+    flash[:warning] = "warning:中午！吃饭啦！"
+  end
 end
